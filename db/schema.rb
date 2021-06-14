ee1033c062593c6a07c937744230a4dd99f494de
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-06-14 12:16:11 +0100

    add comment, delete comment, delete comment status and authentication and security

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_095014) do
+ActiveRecord::Schema.define(version: 2021_06_14_104537) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -25,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_06_14_095014) do
     t.integer "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
